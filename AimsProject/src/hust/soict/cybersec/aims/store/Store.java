3ec0f2c12059e9c3d3e2a0fc419d3aa0610b70bc
Phan Nhat <phandinhnhat1708@gmail.com>
2023-05-14 23:44:04 +0700

    Update the Store class to work with Media

--- AimsProject/src/hust/soict/cybersec/aims/store/Store.java
+++ AimsProject/src/hust/soict/cybersec/aims/store/Store.java
@@ -1,30 +1,41 @@
 package hust.soict.cybersec.aims.store;
 import java.util.ArrayList;
-import java.util.List;
 
-import hust.soict.cybersec.aims.media.DigitalVideoDisc;
+import hust.soict.cybersec.aims.media.Media;
 
 public class Store {
 
-    private List<DigitalVideoDisc> itemsInStore = new ArrayList<DigitalVideoDisc>();
+    private ArrayList<Media> itemsInStore = new ArrayList<Media>();
 
-    public void addDVD(DigitalVideoDisc dvd) {
-        itemsInStore.add(dvd);
-        System.out.println(dvd.getTitle() + " has been added to the store.");
-    }
-    public void removeDVD(DigitalVideoDisc dvd) {
-        boolean removed = itemsInStore.remove(dvd);
-        if (removed) {
-            System.out.println(dvd.getTitle() + " has been removed from the store.");
+    public void addMedia(Media media) {
+        if (itemsInStore.contains(media)) {
+            System.out.println("The media " + media.getTitle() + " is already in the store!");
         } else {
-            System.out.println(dvd.getTitle() + " is not found in the store.");
+            itemsInStore.add(media);
+            System.out.println("The media " + media.getTitle() + " has been added to the store.");
+        }
+    }
+    public void removeMedia(Media media) {
+        if (itemsInStore.remove(media)) {
+            System.out.println("The media " + media.getTitle() + " has been removed from the store.");
+        } else {
+            System.out.println("The media " + media.getTitle() + " is not in the store!");
         }
     }
 
     public void print() {
-        for (int i = 0; i < itemsInStore.size(); i++) {
-            System.out.println(i+1 + ". " + itemsInStore.get(i));
+        if (itemsInStore.size() == 0) {
+            System.out.println("The store is empty!");
+        } else {
+            System.out.println("********************STORE INVENTORY********************");
+            int i = 0;
+            for (Media media : itemsInStore) {
+                i +=1;
+                System.out.println(i +" - " + media);
+            }
+            System.out.println("********************************************************");
         }
     }
+
 
 }
--- AimsProject/src/hust/soict/cybersec/test/store/StoreTest.java
+++ AimsProject/src/hust/soict/cybersec/test/store/StoreTest.java
@@ -7,17 +7,17 @@ public class StoreTest {
         Store store = new Store();
         
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f); 
-        store.addDVD(dvd1);
+        store.addMedia(dvd1);
 
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star War", "Science Fiction", "George Lucas", 87, 24.95f); 
-        store.addDVD(dvd2);
+        store.addMedia(dvd2);
 
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-        store.addDVD(dvd3);
+        store.addMedia(dvd3);
 
         store.print();   
 
-        store.removeDVD(dvd2);
+        store.removeMedia(dvd2);
 
         store.print();   
     }
