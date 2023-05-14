82fe563d45ea82ff0bcd0b05ad35ea8d01910721
Phan Nhat <phandinhnhat1708@gmail.com>
2023-05-14 23:35:20 +0700

    Update the Cart class to work with Media

--- AimsProject/src/hust/soict/cybersec/aims/Aims.java
+++ AimsProject/src/hust/soict/cybersec/aims/Aims.java
@@ -9,13 +9,13 @@ public class Aims {
         Cart anOder = new Cart();
         
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f); 
-        anOder.addDigitalVideoDisc(dvd1);
+        anOder.addMedia(dvd1);
 
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star War", "Science Fiction", "George Lucas", 87, 25.95f); 
-        anOder.addDigitalVideoDisc(dvd2);
+        anOder.addMedia(dvd2);
 
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-        anOder.addDigitalVideoDisc(dvd3);
+        anOder.addMedia(dvd3);
 
         System.out.println("Total cost is: ");
         System.out.println(anOder.totalCost());
--- AimsProject/src/hust/soict/cybersec/test/cart/CartTest.java
+++ AimsProject/src/hust/soict/cybersec/test/cart/CartTest.java
@@ -8,13 +8,13 @@ public class CartTest {
         Cart cart = new Cart();
         
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f); 
-        cart.addDigitalVideoDisc(dvd1);
+        cart.addMedia(dvd1);
 
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star War", "Science Fiction", "George Lucas", 87, 24.95f); 
-        cart.addDigitalVideoDisc(dvd2);
+        cart.addMedia(dvd2);
 
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-        cart.addDigitalVideoDisc(dvd3);
+        cart.addMedia(dvd3);
 
         cart.print();
 
