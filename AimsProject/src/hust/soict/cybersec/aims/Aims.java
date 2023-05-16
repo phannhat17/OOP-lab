3c2e02c0acd18fb1de78c72f591e28cc2cb0e295
Phan Nhat <phandinhnhat1708@gmail.com>
2023-05-16 17:50:19 +0700

    finish the console

--- AimsProject/src/hust/soict/cybersec/aims/Aims.java
+++ AimsProject/src/hust/soict/cybersec/aims/Aims.java
@@ -16,6 +16,7 @@ public class Aims {
         boolean exit = false;
         // CLI
         while (!exit) {
+            
             showMenu();
 
             Scanner scanner = new Scanner(System.in);
@@ -33,7 +34,7 @@ public class Aims {
                     break;
                 case 2:
                     clearConsole();
-                    storeMenu(scanner);
+                    updateStoreMenu(scanner);
                     break;
                 case 3:
                     clearConsole();
@@ -62,8 +63,8 @@ public class Aims {
         store.addMedia(dvd1);
         store.addMedia(dvd2);
         store.addMedia(dvd3);
-        
 
+    
         Book book = new Book("The Valley of Fear", "Detective", 20.00f);
         Book book1 = new Book("A Living Remedy: A Memoir", "Biography", 202.00f);
         Book book2 = new Book("On the Origin of Time: Stephen Hawking's Final Theory", "Science", 120.00f);
@@ -194,9 +195,11 @@ public class Aims {
                     }
                     break;
                 case 4:
+                    clearConsole(); 
                     cartMenu(scanner);
                     break;    
                 default:
+                    clearConsole(); 
                     System.out.println("Invalid option, please choose again.");
                     break;
             }
@@ -230,12 +233,12 @@ public class Aims {
                     }
                     break;
                 default:
+                    clearConsole(); 
                     System.out.println("Invalid option, please choose again.");
                     break;
             }
         }
     }
-
     public static void cartMenu(Scanner scanner) {
         boolean back = false;
         while (!back) {
@@ -258,24 +261,215 @@ public class Aims {
                     back = true;
                     break;
                 case 1:
-                    // 
+                    System.out.println("Filter medias in cart by (1) id or (2) title:");
+                    int filterOption = scanner.nextInt();
+                    scanner.nextLine();
+                    boolean found = false;
+                    while (!found) {
+                        if (filterOption == 1) {
+                            System.out.println("Enter the id to filter (type 0 to stop):");
+                            int id = scanner.nextInt();
+                            scanner.nextLine();
+                            if (id == 0) {
+                                clearConsole();
+                                break;
+                            }
+                            cart.searchByID(id);
+                            found = true;
+                        } else if (filterOption == 2) {
+                            System.out.println("Enter the title to filter (type 0 to stop):");
+                            String title = scanner.nextLine();
+                            if (title.equals("0")) {
+                                clearConsole();
+                                break;
+                            }
+                            cart.searchByTitle(title);
+                            found = true;
+                        } else if (filterOption == 0) {
+                            clearConsole();
+                            break;
+                        } else {
+                            System.out.println("Invalid option.");
+                        }
+                    }
                     break;
                 case 2:
-                    // 
+                    System.out.println("Sort medias in cart by (1) title or (2) cost:");
+                    int sortOption = scanner.nextInt();
+                    scanner.nextLine();
+                    if (sortOption == 1) {
+                        cart.sortMediaByTitle();
+                    } else if (sortOption == 2) {
+                        cart.sortMediaByCost();
+                    } else {
+                        System.out.println("Invalid option.");
+                    }
                     break;
                 case 3:
-                    // 
+                    boolean foundToRemove = false;
+                    while (!foundToRemove) {
+                        System.out.println("Enter the title of the media (type 0 to stop): ");
+                        String title = scanner.nextLine();
+                        if (title.equals("0")) {
+                            clearConsole();
+                            break;
+                        }
+                        Media media = cart.searchToRemove(title);
+                        if (media != null) {
+                            clearConsole();
+                            cart.removeMedia(media);
+                            foundToRemove = true;
+                        } else {
+                            System.out.println("***MEDIA NOT FOUND***");
+                        }
+                    } 
                     break;
                 case 4:
-                    // 
-                    break;
+                    boolean foundToPlay = false;
+                    while (!foundToPlay) {
+                        System.out.println("Enter the title of the media (type 0 to stop): ");
+                        String title = scanner.nextLine();
+                        if (title.equals("0")) {
+                            clearConsole();
+                            break;
+                        }
+                        Media media = store.search(title);
+                        if (media != null) {
+                            if (media instanceof Disc || media instanceof CompactDisc) {
+                                media.play();
+                            } else {
+                                System.out.println("This type of media is not supported!");
+                            }
+                            foundToPlay = true;
+                        } else {
+                            System.out.println("***MEDIA NOT FOUND***");
+                        }
+                    }
+                    break; 
                 case 5:
-                    // 
+                    clearConsole();
+                    cart.empty();
                     break;
                 default:
+                    clearConsole(); 
+                    System.out.println("Invalid option, please choose again.");
+                    break;
+            }
+        }
+    }
+    public static void updateStoreMenu(Scanner scanner) {
+        boolean back = false;
+        while (!back) {
+            System.out.println("Options: ");
+            System.out.println("--------------------------------");
+            System.out.println("1. Add a media");
+            System.out.println("2. Remove a media");
+            System.out.println("0. Back");
+            System.out.println("--------------------------------");
+            System.out.println("Please choose a number: 0-1-2");
+            int option = scanner.nextInt();
+            scanner.nextLine();
+            switch (option) {
+                case 0:
+                    clearConsole();
+                    back = true;
+                    break;
+                case 1:
+                    System.out.println("Enter the category of the media (1) Book, (2) CD, (3) DVD or (0) exit:");
+                    int categoryChoice = scanner.nextInt();
+                    scanner.nextLine();
+                    
+                    if (categoryChoice == 1) {
+                        System.out.println("Enter book title: ");
+                        String bookTitle = scanner.nextLine();
+                        System.out.println("Enter book category: ");
+                        String bookCategory = scanner.nextLine();
+                        System.out.println("Enter book cost: ");
+                        Float bookCost = scanner.nextFloat();
+                        scanner.nextLine();
+
+                        Book newBook = new Book(bookTitle, bookCategory, bookCost);
+                        store.addMedia(newBook);
+                    } else if (categoryChoice == 2) {
+                        System.out.println("Enter CD title: ");
+                        String cdTitle = scanner.nextLine();
+                        System.out.println("Enter CD category: ");
+                        String cdCategory = scanner.nextLine();
+                        System.out.println("Enter CD artist: ");
+                        String cdArtist = scanner.nextLine();
+                        System.out.println("Enter CD cost: ");
+                        Float cdCost = scanner.nextFloat();
+                        scanner.nextLine();
+
+                        CompactDisc newCD = new CompactDisc(cdTitle, cdCategory, cdArtist, cdCost);
+
+                        
+                        System.out.println("Do you want to add tracks to your CD? (1) Yes (0) No:");
+                        int addTrack = scanner.nextInt();
+                        scanner.nextLine();
+                        
+                        if (addTrack == 1) {
+                            System.out.println("How many tracks in your CD?");
+                            int numTrack = scanner.nextInt();
+                            scanner.nextLine();
+                            for (int i = 0; i < numTrack; i++) {
+                                System.out.println("Your " + (i+1) + " track: ");
+                                System.out.println("Enter track title: ");
+                                String trackTitle = scanner.nextLine();
+                                System.out.println("Enter track length: ");
+                                int trackLength = scanner.nextInt();
+                                scanner.nextLine();
+
+                                Track newTrack = new Track(trackTitle, trackLength);
+                                newCD.addTrack(newTrack);
+                            }
+                            store.addMedia(newCD);
+                        } else if (addTrack == 0) {
+                            store.addMedia(newCD);
+                        }
+                    } else if (categoryChoice == 3) {
+                        System.out.println("Enter DVD title: ");
+                        String dvdTitle = scanner.nextLine();
+                        System.out.println("Enter DVD category: ");
+                        String dvdCategory = scanner.nextLine();
+                        System.out.println("Enter book cost: ");
+                        Float dvdCost = scanner.nextFloat();
+                        scanner.nextLine();
+                        
+                        DigitalVideoDisc newDVD = new DigitalVideoDisc(dvdTitle, dvdCategory, dvdCost);
+                        store.addMedia(newDVD);                
+                    } else if (categoryChoice == 0) {
+                        clearConsole();
+                        break;
+                    } else {
+                        System.out.println("Invalid option.");
+                    }
+                    break;
+                case 2:
+                    boolean foundToRemove = false;
+                    while (!foundToRemove) {
+                        System.out.println("Enter the title of the media (type 0 to stop): ");
+                        String titleForRemove = scanner.nextLine();
+                        if (titleForRemove.equals("0")) {
+                            clearConsole();
+                            break;
+                        }
+                        Media media = store.search(titleForRemove);
+                        if (media != null) {
+                            clearConsole();
+                            store.removeMedia(media);
+                            foundToRemove = true;
+                        } else {
+                            System.out.println("***MEDIA NOT FOUND***");
+                        }
+                    }
+                    break;
+                default:
+                    clearConsole();
                     System.out.println("Invalid option, please choose again.");
                     break;
             }
         }
     }
 }
+        
