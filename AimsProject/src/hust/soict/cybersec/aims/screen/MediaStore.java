e375b1c6d4916ea1a252fa28d894a51b883d2e9a
phannhat17 <phandinhnhat1708@gmail.com>
2023-05-18 21:05:29 +0700

    finish the static GUI

---
+++ AimsProject/src/hust/soict/cybersec/aims/screen/MediaStore.java
@@ -0,0 +1,45 @@
+package hust.soict.cybersec.aims.screen;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+
+import hust.soict.cybersec.aims.store.Store;
+import hust.soict.cybersec.aims.media.*;
+
+
+public class MediaStore extends JPanel {
+    
+    private Media media;
+
+    public MediaStore(Media media) {
+
+        this.media = media;
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JLabel title = new JLabel(media.getTitle());
+        title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 20));
+        title.setAlignmentX(CENTER_ALIGNMENT);
+
+        JLabel cost = new JLabel(""+media.getCost()+"$");
+        cost.setAlignmentX(CENTER_ALIGNMENT);
+
+        JPanel container = new JPanel();
+        container.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        container.add(new JButton("Add to cart"));
+        if (media instanceof Playable) {
+            container.add(new JButton("Play"));
+        }
+
+        this.add(Box.createVerticalGlue());
+        this.add(title);
+        this.add(cost);
+        this.add(Box.createVerticalGlue());
+        this.add(container);
+
+        this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
+
+}
