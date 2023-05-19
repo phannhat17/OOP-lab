7e1eee012bcafe16e3d6c690610db71e8ac98e04
Phan Nhat <phandinhnhat1708@gmail.com>
2023-05-19 14:32:38 +0700

    finish painter gui

--- GUIProject/src/hust/soict/cybersec/javafx/PainterController.java
+++ GUIProject/src/hust/soict/cybersec/javafx/PainterController.java
@@ -1,47 +1,42 @@
 package hust.soict.cybersec.javafx;
 
-import javafx.event.ActionEvent; 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.print.Collation;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 
 public class PainterController {
-	@FXML
-	private Pane drawingAreaPane;
-	
-	@FXML
-	private RadioButton pen;
-	
-	@FXML
-	private RadioButton eraser;
-	
-	@FXML
-	private ToggleGroup drawTools;
-	
-	
-	@FXML
-	void clearButtonPressed(ActionEvent event) {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Pane drawingAreaPane;
+
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
 		drawingAreaPane.getChildren().clear();
-	}
-	
-	@FXML
-	void drawingAreaMouseDragged(MouseEvent event) {
-		Rectangle clipArea = new Rectangle(0, 0, drawingAreaPane.getWidth(), drawingAreaPane.getHeight());
-		drawingAreaPane.setClip(clipArea);
-		if (pen.isSelected() || eraser.isSelected()) {
-			Color inkColor;
-			if (pen.isSelected()) {
-				inkColor = Color.BLACK;
-			} else {
-				inkColor = Color.WHITE;
-			}
-			Circle newCircle = new Circle(event.getX(), event.getY(), 4, inkColor);
-			drawingAreaPane.getChildren().add(newCircle);
-		}
-	}
+    }
+
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event) {
+		Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
+		drawingAreaPane.getChildren().add(newCircle);
+    }
+
+    @FXML
+    void initialize() {
+        assert drawingAreaPane != null : "fx:id=\"drawingAreaPane\" was not injected: check your FXML file 'Painter.fxml'.";
+
+    }
+
 }
