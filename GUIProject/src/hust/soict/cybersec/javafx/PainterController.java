237784a235762d03f30dc73892373ea956812369
Phan Nhat <phandinhnhat1708@gmail.com>
2023-05-19 15:16:22 +0700

    add the Eraser feature

--- GUIProject/src/hust/soict/cybersec/javafx/PainterController.java
+++ GUIProject/src/hust/soict/cybersec/javafx/PainterController.java
@@ -5,13 +5,22 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.print.Collation;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 public class PainterController {
+
+    @FXML
+    private RadioButton eraser;
+
+    @FXML
+    private RadioButton pen;
 
     @FXML
     private ResourceBundle resources;
@@ -24,13 +33,19 @@ public class PainterController {
 
     @FXML
     void clearButtonPressed(ActionEvent event) {
-		drawingAreaPane.getChildren().clear();
+		  drawingAreaPane.getChildren().clear();
     }
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-		Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
-		drawingAreaPane.getChildren().add(newCircle);
+      Rectangle clipArea = new Rectangle(0, 0, drawingAreaPane.getWidth(), drawingAreaPane.getHeight());
+      drawingAreaPane.setClip(clipArea);
+      Color inkColor = Color.BLACK;
+      if (eraser.isSelected()) {
+        inkColor = Color.WHITE;
+      } 
+      Circle newCircle = new Circle(event.getX(), event.getY(), 4, inkColor);
+      drawingAreaPane.getChildren().add(newCircle);
     }
 
     @FXML
