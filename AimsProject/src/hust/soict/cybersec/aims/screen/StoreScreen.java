ee2eaf994a9ae6f1ef63d6b5737d0665e37b14cc
Phan Nhat <phandinhnhat1708@gmail.com>
2023-05-23 12:28:45 +0700

    add PlayerException in DVD

--- AimsProject/src/hust/soict/cybersec/aims/screen/StoreScreen.java
+++ AimsProject/src/hust/soict/cybersec/aims/screen/StoreScreen.java
@@ -20,7 +20,7 @@ public class StoreScreen extends JFrame {
 
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);     
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star War", "Science Fiction", "George Lucas", 87, 24.95f); 
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", null, 0, 18.99f);
         store.addMedia(dvd1);
         store.addMedia(dvd2);
         store.addMedia(dvd3);
